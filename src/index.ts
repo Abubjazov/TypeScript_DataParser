1b6c0c47ed120edc66bfc16107316a20647beac6
Radik Abubjazov <abubjazov@mail.ru>
2021-12-29 01:23:17 +0300

    proj: +convert .CSV file to Array

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import fs from 'fs'
 const matches = fs.readFileSync('data_source/football.csv', {
     encoding: 'utf-8'
 })
+.split('\n')
+.map((item: string): string[] => item.split(','))
 
 console.log(matches)
