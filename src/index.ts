c311a20a0d25ce4d579589f849bdb046b7906856
Radik Abubjazov <abubjazov@mail.ru>
2021-12-29 01:57:14 +0300

    proj: refactor by Enum

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,24 @@ const matches = fs.readFileSync('data_source/football.csv', {
 .split('\n')
 .map((item: string): string[] => item.split(','))
 
-// console.log(matches)
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+}
+
+const getMatchResult = (match: string[]): MatchResult => {
+    if (match[5] === 'H') return MatchResult.HomeWin
+    if (match[5] === 'A') return MatchResult.AwayWin
+    if (match[5] === 'D') return MatchResult.Draw
+    
+    throw new Error('Unexpected argument')
+}
+
 let wins = 0
 
 for (let match of matches) {
-    if ((match[1] === 'Man United' && match[5] === 'H') || (match[2] === 'Man United' && match[5] === 'A')) {
+    if ((match[1] === 'Man United' && match[5] === MatchResult.HomeWin) || (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)) {
         wins++
     }
 }
