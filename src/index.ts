f56591efeff03369794c33404d258564e1a0d2f8
Radik Abubjazov <abubjazov@mail.ru>
2021-12-29 01:34:42 +0300

    proj: +parse dataArray

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,13 @@ const matches = fs.readFileSync('data_source/football.csv', {
 .split('\n')
 .map((item: string): string[] => item.split(','))
 
-console.log(matches)
+// console.log(matches)
+let wins = 0
+
+for (let match of matches) {
+    if ((match[1] === 'Man United' && match[5] === 'H') || (match[2] === 'Man United' && match[5] === 'A')) {
+        wins++
+    }
+}
+
+console.log(`Your team wins: ${ wins }`)
