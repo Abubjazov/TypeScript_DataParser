08083c5b1bf141883287de1222becacade3464bb
Radik Abubjazov <abubjazov@mail.ru>
2021-12-29 21:44:48 +0300

    proj: refactor by class CsvFileReader

---
+++ src/classes/CsvFileReader.ts
@@ -0,0 +1,16 @@
+import fs from 'fs'
+import path from 'path'
+
+export class CsvFileReader {
+    data: string[][] = []
+
+    constructor(public filename: string = filename) {}
+
+    read(): void {
+        this.data = fs.readFileSync(path.join(__dirname, '../..', `data_source/${this.filename}`), {
+            encoding: 'utf-8'
+        })
+        .split('\n')
+        .map((item: string): string[] => item.split(','))
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-import fs from 'fs'
-
-const matches = fs.readFileSync('data_source/football.csv', {
-    encoding: 'utf-8'
-})
-.split('\n')
-.map((item: string): string[] => item.split(','))
+import { CsvFileReader } from './classes/CsvFileReader'
 
 enum MatchResult {
     HomeWin = 'H',
@@ -12,17 +6,12 @@ enum MatchResult {
     Draw = 'D'
 }
 
-const getMatchResult = (match: string[]): MatchResult => {
-    if (match[5] === 'H') return MatchResult.HomeWin
-    if (match[5] === 'A') return MatchResult.AwayWin
-    if (match[5] === 'D') return MatchResult.Draw
-    
-    throw new Error('Unexpected argument')
-}
+const data = new CsvFileReader('football.csv')
+data.read() 
 
 let wins = 0
 
-for (let match of matches) {
+for (let match of data.data) {
     if ((match[1] === 'Man United' && match[5] === MatchResult.HomeWin) || (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)) {
         wins++
     }
